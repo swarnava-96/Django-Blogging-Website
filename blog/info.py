788d56ac8bcebd240cd5d9913c94b3bde7eb8613
Swarnava Mukherjee <75041273+swarnava-96@users.noreply.github.com>
2021-11-10 22:31:35 +0530

    Update info.py

--- blog/info.py
+++ blog/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'swarnava712@gmail.com'
-EMAIL_HOST_PASSWORD = 'KOL@P123456'
+EMAIL_HOST_USER = '#####'
+EMAIL_HOST_PASSWORD = '####'
 EMAIL_PORT = 587
